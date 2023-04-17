271e1186964b85b3c0f381e9e07968c3a76d14e7
marianitadileo <mdileo36@gmail.com>
2023-04-17 15:44:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+
+<template>
+    <header>
+        <h1>Text</h1>
+    </header>
+</template>
+
+<style>
+header {
+    background-color: lightblue;
+    height: 100px;
+    width: 100%;
+}
+</style>
